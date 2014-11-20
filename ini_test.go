c63a65395c9e350f11d9f03ae4a4d5f1fed07c5b
Steve Spencer <sspencer@playnetwork.com>
2014-11-20 06:40:34 -0800

    Put struct def within Test func.  Another another field member to struct.

--- ini_test.go
+++ ini_test.go
@@ -4,19 +4,22 @@ import (
 	"testing"
 )
 
-type Parse1 struct {
-	StartCmd struct {
-		Foo string `ini:"FOO"`
-	} `ini:"[START]"`
-}
-
 func TestParse1(t *testing.T) {
+
+	type Parse struct {
+		StartCmd struct {
+			Foo   string `ini:"FOO"`
+			Magic int    `ini:"Magic Number"`
+		} `ini:"[START]"`
+	}
+
 	b := []byte(`
 ; ignore me
 [START]
-FOO=BAR`)
+FOO=BAR
+Magic Number = 42`)
 
-	var d Parse1
+	var d Parse
 	err := Unmarshal(b, &d)
 
 	if err != nil {
@@ -25,5 +28,7 @@ FOO=BAR`)
 
 	if d.StartCmd.Foo != "BAR" {
 		t.Fatal("Field FOO not set")
+	} else if d.StartCmd.Magic != 42 {
+		t.Fatal("Field Magic not set")
 	}
 }
